31ff8708ba41d411f6ed32e00b9447bb6df949c7
Samuel <samuel.toledo@discente.ufg.br>
2024-09-23 23:29:54 -0300

    módulo e script do NAO

---
+++ NAO.py
@@ -0,0 +1,27 @@
+import json
+import python3
+
+while True:
+
+
+    #conexão via socket 
+    python3.llm_server()
+
+    #puxa a pergunta e roda a llm 
+    pergunta = python3.audio_to_text("/home/samuel/Projeto_NAO/gravacao.wav")
+    print(pergunta)
+    response = python3.consultar_chatgpt(pergunta)
+    print(response)
+
+    #limpa o histórico de conversa e fecha a conexão
+    if response == "tchau":
+        python3.limpar_historico
+        python3.socket.close()   
+        break
+
+
+    else:
+        # Salvar a string em um arquivo JSON
+        file_path = ('data.json')
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump({"message": response}, file, ensure_ascii=False, indent=4)
--- python3.py
+++ python3.py
@@ -1,10 +1,35 @@
 import speech_recognition as sr
 from openai import OpenAI
-
+import socket
 
 
 #Define o comportamento do chat e armazena as informações da conversa
 historico = [{"role": "system", "content": "Você é um robô chamado NAO e seu objetivo é interagir com pessoas importantes em eventos sobre tecnologia"}]
+
+
+def llm_server():
+    """FUNÇÃO QUE ESTABELECE CONEXÃO COM O NAO ATRAVÉS DE UM SOCKET SERVIDOR."""
+
+
+    #cria e configura o servidor socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', 6000))  # Abre o servidor na porta 6000
+    server_socket.listen(1)
+    print("Servidor aguardando conexões")
+
+
+    #aguarda a conexão com o  NAO
+    conn, addr = server_socket.accept()
+    print(f"Connected to: {addr}")
+
+
+    #sinal de ativação
+    var_ativacao = conn.recv(1024).decode()
+    print(f"Received from Python 2.7: {var_ativacao}")
+
+
+    var_resposta = ('ok')
+    conn.send(var_resposta.encode('utf-8'))
 
 
 
@@ -17,20 +42,24 @@ def audio_to_text(audio_file):
     #Cria o objeto que realiza o reconhecimento
     r = sr.Recognizer()
 
+
     # Carrega o arquivo de áudio
     with sr.AudioFile(audio_file) as source:
         audio = r.record(source)
+
 
     try:
         # Realiza o reconhecimento de fala
         text = r.recognize_google(audio, language='pt-BR')  # Substitua 'pt-BR' pelo idioma desejado
         return text
     
+
     #tratamentos de erros
     except sr.UnknownValueError:
         print("Não foi possível reconhecer o áudio")
     except sr.RequestError as e:
         print("Erro do serviço de reconhecimento de fala; {0}".format(e))
+
 
 
 
@@ -43,16 +72,20 @@ def consultar_chatgpt(texto):
     #inicia o cliente da API através da chave de api
     client = OpenAI(api_key="CHAVE_API!")
 
+
     #armazena o texto entregue como parâmetro como contéudo enviado pelo usuário
     historico.append({"role": "user" , "content": texto})
+
 
     #define o modelo do chat e utiliza o "histórico" como contexto
     response = client.chat.completions.create(
     model="gpt-4o",
     messages= historico)
 
+
     #responde a mensagem considerando o contexto
     resposta_chatgpt = response.choices[0].message.content
+
 
     #armazena a resposta do chat como contéudo enviado pelo assistente (próprio chat)
     historico.append({"role": "assistant", "content": resposta_chatgpt})
@@ -63,6 +96,7 @@ def consultar_chatgpt(texto):
 
 def limpar_historico():
     """FUNÇÃO QUE RESETA  HISTÓRICO DO CHAT PARA O PADRÃO"""
+
 
     global historico  # Usando 'global' para modificar a variável global
     historico = [{"role": "system", "content": "Você é um robô chamado NAO e seu objetivo é interagir com pessoas importantes em eventos sobre tecnologia."}]
